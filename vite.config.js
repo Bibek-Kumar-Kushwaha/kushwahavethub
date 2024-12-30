689cc04012a13be184a0bee3e90bf4ddfda670f8
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-12-30 21:11:44 +0545

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allows access from the local network
-    port: 5173, // Optional: Default port is 5173
-    strictPort: true, // Ensures the specified port is used
-  },
-})
+});
